d9517b3c97aceb37953bed15ab81c93758571c04
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2021-06-09 16:50:17 -0700

    Add files via upload

--- guessingGame.py
+++ guessingGame.py
@@ -7,13 +7,17 @@ while tries < 5:
     guess = int(input("the number is between 1,9: "))
     if guess > number:
         print('too high, try again')
+        
     if guess < number:
         print('too low, try again')
+        
     if guess == number:
         break
+    
+    tries = tries + 1
 if guess == number:
     print("CONGRADULATIONS YOU WON!!!")
-elif guess != number:
+else:
     print("You lost, the number was ", number)
 
     
